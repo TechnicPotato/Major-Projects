b7fa4e81e2e62e3f361150ec89cf2582ead2aa3a
Bill <ZhuangBill@users.noreply.github.com>
2019-12-26 14:43:49 +1100

    Moved read and write to new functions

--- basic_db/basic_db.py
+++ basic_db/basic_db.py
@@ -1,7 +1,22 @@
 import yaml
 import os
 
+def load_data():
+    """ Opens yaml file and returns data as dict"""
+    with open("basic_db\\basic_db.yaml", "r") as f:
+        yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+    if yaml_data != None:
+        return yaml_data
+    return None
+
+def write_data(data):
+    """ Writes data to a yaml file"""
+    with open("basic_db\\basic_db.yaml", "w") as f:
+        yaml.dump(data, f, sort_keys=True)
+
+
 def user_input(data):
+    """ Takes user input and adds to YAML file"""
     print("Inputting new entry.")
     name = input("Input name:\t")
     author = input("Input author:\t")
@@ -23,13 +38,6 @@ def user_input(data):
     return data
 
 if __name__ == "__main__":
+    # Create an empty dict in case there is no yaml_data
     data = dict()
-    with open("basic_db\\basic_db.yaml", "r") as f:
-        yaml_data = yaml.load(f, Loader=yaml.FullLoader)
-    # Update the yaml_data and prevent None Type Errors
-    if yaml_data != None:
-        data = yaml_data
-    print(repr(data))
-    data = user_input(data)
-    with open("basic_db\\basic_db.yaml", "w") as f:
-        yaml.dump(data, f, sort_keys=True)
+    
