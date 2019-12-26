604027a2a11406b279b0260f6854ec9fa108f3cd
Bill <ZhuangBill@users.noreply.github.com>
2019-12-26 14:34:38 +1100

    Added basic file io for yaml

--- basic_db/basic_db.py
+++ basic_db/basic_db.py
@@ -1,13 +1,14 @@
 import yaml
+import os
 
-def user_input():
+def user_input(data):
     print("Inputting new entry.")
     name = input("Input name:\t")
     author = input("Input author:\t")
     year = input("Input year:\t")
     tags = input("Input tags, seperated by commas:\n").split(",")
     text = input("Input text:\n")
-    data = dict(
+    current_data = dict(
         name = dict(
             author = author,
             year = year,
@@ -16,9 +17,19 @@ def user_input():
         )
     )
     # Replace name with actual name of string
-    data[str(name)] = data['name']
-    del data['name']
-    print(data)
+    current_data[str(name)] = current_data['name']
+    del current_data['name']
+    data.update(current_data)
+    return data
 
 if __name__ == "__main__":
-    user_input()
+    data = dict()
+    with open("basic_db\\basic_db.yaml", "r") as f:
+        yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+    # Update the yaml_data and prevent None Type Errors
+    if yaml_data != None:
+        data = yaml_data
+    print(repr(data))
+    data = user_input(data)
+    with open("basic_db\\basic_db.yaml", "w") as f:
+        yaml.dump(data, f, sort_keys=True)
