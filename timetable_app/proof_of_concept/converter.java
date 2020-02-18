45cbf64ea7d2152c1b2b2159860dbb34de3dea0f
Bill <46290332+TechnicPotato@users.noreply.github.com>
2020-02-18 14:08:27 +1100

    Added Framework for Java App

---
+++ timetable_app/proof_of_concept/converter.java
@@ -0,0 +1,17 @@
+// Import needed modules
+// <--- I/O --->
+// Go to website and retrieve data
+// <--- Event Parsing -->
+// From retrieved data, sort into events
+// Capability to retrieve events when needed
+// <--- Text storage --->
+// Store data as a file
+// <--- Possibly store a current event --->
+// Sort events such that they are checked against system time
+// Ability to skip to the next event
+// Display time til next event
+// Possibly display next event
+// <--- UI Design --->
+// Basic Swipe controls
+// Design an icon
+
