1d62b82dbd97d9b0c37050c6a3d7f548fb9fb6d3
Bill <46290332+TechnicPotato@users.noreply.github.com>
2020-02-19 14:59:04 +1100

    Added new RetrieveTimetable function for I/O

--- timetable_app/proof_of_concept/converter.java
+++ timetable_app/proof_of_concept/converter.java
@@ -1,17 +1,41 @@
 // Import needed modules
-// <--- I/O --->
-// Go to website and retrieve data
-// <--- Event Parsing -->
-// From retrieved data, sort into events
-// Capability to retrieve events when needed
-// <--- Text storage --->
-// Store data as a file
-// <--- Possibly store a current event --->
-// Sort events such that they are checked against system time
-// Ability to skip to the next event
-// Display time til next event
-// Possibly display next event
-// <--- UI Design --->
-// Basic Swipe controls
-// Design an icon
+import java.net.URL;
+import java.util.Scanner;
+
+public class converter {
+    public static void main(String[] args){
+        converter test = new converter();
+        // <--- I/O --->
+        test.RetrieveTimetable("http://www.timetable.usyd.edu.au/personaltimetable/timetable/calendar/490481932/xp92hv2doHgqJh9LEudtX9sa8uNUcZBUuwtE72XYzp9/timetable.ics");
+    }
+    public String RetrieveTimetable(String url) {
+        try {
+            URL timetableurl = new URL(url);
+            Scanner s = new Scanner(timetableurl.openStream(), "UTF-8");
+            while (s.hasNext()){
+                System.out.println(s.next());
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return "Test";
+    }
+}
+    // Go to website and retrieve data
+    // <--- Event Parsing -->
+    // From retrieved data, sort into events
+    // Capability to retrieve events when needed
+    // <--- Text storage --->
+    // Store data as a file
+    // <--- Possibly store a current event --->
+    // Sort events such that they are checked against system time
+    // Ability to skip to the next event
+    // Display time til next event
+    // Possibly display next event
+    // <--- UI Design --->
+    // Basic Swipe controls
+    // Design an icon
+
 
