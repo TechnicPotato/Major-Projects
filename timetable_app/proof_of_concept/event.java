ba222e8c738e0ead5df56ae1198544844dacf17c
Bill <46290332+TechnicPotato@users.noreply.github.com>
2020-02-19 16:22:45 +1100

    Added an event class and constructor

---
+++ timetable_app/proof_of_concept/event.java
@@ -0,0 +1,14 @@
+public class event {
+    String name;
+    String dtstart;
+    String dtend;
+    String uid;
+    String location;
+    public event(String name, String dtstart, String dtend, String uid, String location) {
+        this.name = name;
+        this.dtstart = dtstart;
+        this.dtend = dtend;
+        this.uid = uid;
+        this.location = location;
+    }
+}
