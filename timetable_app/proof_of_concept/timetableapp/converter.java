86adf6252f2153bb8219a3ec704d5d2bbb969873
Bill <ZhuangBill@users.noreply.github.com>
2020-02-23 15:25:24 +1100

    Added basic exception handling to retrieve timetable event

--- timetable_app/proof_of_concept/timetableapp/converter.java
+++ timetable_app/proof_of_concept/timetableapp/converter.java
@@ -8,8 +8,16 @@ import timetableapp.Event;
 public class Converter {
     public static void main(String[] args){
         Converter test = new Converter();
+        String output;
         // <--- I/O --->
-        String output = test.RetrieveTimetable("http://www.timetable.usyd.edu.au/personaltimetable/timetable/calendar/490481932/xp92hv2doHgqJh9LEudtX9sa8uNUcZBUuwtE72XYzp9/timetable.ics");
+        // Perform basic checking
+        try {
+            output = test.RetrieveTimetable("http://www.timetable.usyd.edu.au/personaltimetable/timetable/calendar/490481932/xp92hv2doHgqJh9LEudtX9sa8uNUcZBUuwtE72XYzp9/timetable.ics");
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
         String[] bar = output.split("\n");
         // String teststring = bar[30]+"\n"+bar[31]+"\n"+bar[32]+"\n"+bar[33]+"\n"+bar[34]+"\n"+bar[35]+"\n"+bar[36];
         // try {
@@ -25,7 +33,7 @@ public class Converter {
         // }
     }
     // TODO: Add to seperate methods file
-    public String RetrieveTimetable(String url) {
+    public String RetrieveTimetable(String url) throws Exception{
         // Attempts to go to a website and retrieve the data there. Returns as a string form, assumes UTF-8 formatting
         try {
             URL timetableurl = new URL(url);
@@ -36,7 +44,14 @@ public class Converter {
             }
             // Prevent a memory leak
             s.close();
-            return outputstr.toString();
+            String output = outputstr.toString();
+            // Validation checking
+            if (CheckCalendarValidity(output)) {
+                return output;
+            }
+            else {
+                throw new Exception("Calendar not in specified ical format!");
+            }
         }
         catch (Exception e) {
             // Calling a failure
@@ -55,7 +70,7 @@ public class Converter {
      * @return A boolean if the timetablestring matches the preferred format.
      * @see Converter#RetrieveTimetable(String)
      */
-    private boolean CheckCalenderValidity(String timetablestring) {
+    private static boolean CheckCalendarValidity(String timetablestring) {
         if (timetablestring.startsWith("BEGIN:VCALENDAR") && timetablestring.endsWith("END:VCALENDAR")) {
             return true;
         }
